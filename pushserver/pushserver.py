21682c83f15da5d57647abc60fbe1d57c5e64a0d
Chris Culnane <chrisculnane@gmail.com>
2022-03-16 19:05:07 +0000

    Additional comments

--- pushserver/pushserver.py
+++ pushserver/pushserver.py
@@ -1,15 +1,50 @@
+"""
+ © Copyright 2021-2022 University of Surrey
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice,
+ this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+
+"""
+
+
+import json
 import logging
 import os
-from flask import Flask, request, g, jsonify,Response
 import sqlite3
-import json
+
 import firebase_admin
 from firebase_admin import messaging
+from firebase_admin.exceptions import FirebaseError
+from flask import Flask, Response, g, jsonify, request
+
+#Paths to database and schema
 DATABASE = './data/db.sqlite'
 SCHEMA = "./schema.sql"
+
 default_app = firebase_admin.initialize_app()
 
 app = Flask(__name__)
+
+#Logging Path
 if not os.path.exists("./logs/"):
     os.mkdir("./logs/")
 logging.basicConfig(filename='./logs/access.log', level=logging.DEBUG)
@@ -41,9 +76,9 @@ def make_dicts(cursor, row):
 def query_db(query, args=(), one=False):
     """Example from Flask documentation for easy querying
     Args:
-        query ([type]): [description]
-        args (tuple, optional): [description]. Defaults to ().
-        one (bool, optional): [description]. Defaults to False.
+        query (str): Query string to run
+        args (tuple, optional): arguments to pass to db execute. Defaults to ().
+        one (bool, optional): True to only return one. Defaults to False.
     Returns:
         Either a list or a single row if only one
     """
@@ -72,6 +107,17 @@ def init_db(db: sqlite3.Connection):
 
 @app.route("/register", methods=['POST'])
 def register():
+    """Register endpoint. Receives POST requests from
+    Companion Devices to register their Firebase ID
+    against their Public Key ID
+
+    This will overwrite an existing FirebaseID for the 
+    specified Public Key ID
+
+    Returns:
+        JSON: success:True if successful, False if not
+    """    """
+    """
     content = request.get_json()
     firebaseId = content["fb_id"]
     companion_public_key = content["pub_key"]
@@ -93,27 +139,48 @@ def register():
         logging.error("Failed to register user", exc_info=True)
         return jsonify(success=False)
 
-    return "<p>Hello, World!</p>"
 
 
 @app.route("/pushmessage", methods=['POST'])
 def pushmessage():
+    """Routes a message from the sender to the receiver via
+    Firebase Cloud Messaging using the Firebase DeviceID 
+    associated with the target public key. 
+    
+    Returns 500 if the target device is not found
+
+    Returns:
+        JSON: success:True or 500 error
+    """
     content = request.get_json()
     msg = content["msg"]
     companion_public_key = content["pub_key"]
     device = query_db('SELECT * from devices WHERE device_pub_key = ?', (companion_public_key,), one=True)
     if device is None:
         return jsonify(Response(500,"Device not found"))
-    send_to_fcm(device["firebase_id"],msg)
+    try:
+        send_to_fcm(device["firebase_id"],msg)
+    except FirebaseError:
+        logging.error("Error sending to firebase", exc_info=True)
+        return jsonify(success=False)    
+    except ValueError:
+        logging.error("ValueError sending to firebase", exc_info=True)
+        return jsonify(success=False)    
     return jsonify(success=True)
 
 
 def send_to_fcm(target, msg:dict):
+    """Internal function to send a message via Firebase
+    Cloud Messaging
 
+    Args:
+        target (str): target FCM device ID
+        msg (dict): JSON message to send
+    """
     # This registration token comes from the client FCM SDKs.
     registration_token = target
     
-    # See documentation on defining a message payload.
+    #Payload sent with high priority
     message = messaging.Message(
         data=msg,
         token=registration_token,
